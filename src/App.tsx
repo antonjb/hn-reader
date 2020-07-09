95c9a371ff45d88b6d1c421ab94ea4ef4c35a2e5
Anton Ball <antonball@gmail.com>
2020-07-09 21:01:03 +0800

    Creates Story component
    The basic structure of what a story will look like. The skeleton will be
    used during the loading phase of the Story

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Status } from './components/Status/Status'
+import { Story } from './components/Story/Story'
 
 export const App: React.FC = () => (
     <div>
         <Status />
-        App goes here...
+        <Story
+            title="Great CSS Frameworks for Web Developers"
+            url="https://insights.dice.com/2020/07/09/5-great-css-frameworks-for-web-developers/"
+            author="Jane Smith"
+            publicationDate={1594298350}
+        />
     </div>
 )
 App.displayName = 'App'
